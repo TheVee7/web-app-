d629b177f5da82f1c441b943d6ada356fec57c22
TheVee7 <vs8273852624@gmail.com>
2024-08-03 22:06:51 +0530

    COmpleted for base

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
@@ -13,6 +13,14 @@ def OrderDHA():
 @app.route("/OrderAssignments")
 def OrderAssignment():
     return render_template("Assignment.html")
+
+@app.route("/MoreOption")
+def MoreOption():
+    return render_template("more_opt.html")
+
+@app.route('/download')
+def download():
+    return send_from_directory('static', 'TheHelpingApp.apk', as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port= 5000)
